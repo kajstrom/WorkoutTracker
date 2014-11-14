297611d539f81960f0933f62a7c1bcdcc679c7a7
Kaj Strom <kaj.strom@kapsi.fi>
2014-11-14 07:58:03 +0200

    Updated Gruntfile.js to only use Uglify. Disabled compression of vendor files as there did not work for some reason. Application files worked fine.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,29 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            vendor: {
-                src: [
-                    "bower_components/jquery/dist/jquery.js",
-                    "bower_components/underscore/underscore.js",
-                    "bower_components/backbone.babysitter/lib/backbone.babysitter.js",
-                    "bower_components/backbone.wreqr/lib/backbone.wreqr.js",
-                    "bower_components/marionette/lib/core/backbone.marionette.js",
-                    "bower_components/backbone.syphon/lib/backbone.syphon.js"
-                ],
-                dest: 'dist/js/vendor.js'
-            },
-            app: {
-                src: ['app/assets/js/**/*.js'],
-                dest: 'dist/js/app.js'
-            }
-        },
         uglify: {
             options: {
-                mangle: false
+                mangle: false,
+				sourceMap: true,
+				compress: true
             },
             my_target: {
                 files: {
-                    'dist/js/app.min.js': ['dist/js/app.js'],
-                    'dist/js/vendor.min.js': ['dist/js/vendor.js']
+                    'dist/js/app.min.js': ['app/assets/js/**/*.js']
+                    /*'dist/js/vendor.min.js': [
+						"bower_components/jquery/dist/jquery.js",
+						"bower_components/underscore/underscore.js",
+						"bower_components/backbone.babysitter/lib/backbone.babysitter.js",
+						"bower_components/backbone.wreqr/lib/backbone.wreqr.js",
+						"bower_components/marionette/lib/core/backbone.marionette.js",
+						"bower_components/backbone.syphon/lib/backbone.syphon.js"
+					]*/
                 }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('build', ['concat', 'uglify']);
+    grunt.registerTask('build', ['uglify']);
 };
