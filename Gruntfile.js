51ac8e874d88b25b3ca95be67dfe5eefb3ae3113
Kaj Strom <kaj.strom@kapsi.fi>
2014-11-15 19:58:10 +0200

    Added backbone to vendor file minifying.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,15 +9,16 @@ module.exports = function(grunt) {
             },
             my_target: {
                 files: {
-                    'dist/js/app.min.js': ['app/assets/js/**/*.js']
-                    /*'dist/js/vendor.min.js': [
+                    'dist/js/app.min.js': ['app/assets/js/**/*.js'],
+                    'dist/js/vendor.min.js': [
 						"bower_components/jquery/dist/jquery.js",
 						"bower_components/underscore/underscore.js",
+						"bower_components/backbone/backbone.js",
 						"bower_components/backbone.babysitter/lib/backbone.babysitter.js",
 						"bower_components/backbone.wreqr/lib/backbone.wreqr.js",
 						"bower_components/marionette/lib/core/backbone.marionette.js",
 						"bower_components/backbone.syphon/lib/backbone.syphon.js"
-					]*/
+					]
                 }
             }
         }
