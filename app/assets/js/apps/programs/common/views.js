1d47696f60dd89314647aa848c1b3339f5e550b8
Ville Viklund <ville.viklund@gmail.com>
2014-11-29 14:57:41 +0200

    Programs and program exercises can be saved.

--- app/assets/js/apps/programs/common/views.js
+++ app/assets/js/apps/programs/common/views.js
@@ -4,7 +4,8 @@ WorkoutTracker.module("ProgramsApp.Common.Views", function (Views, WorkoutTracke
         className: "row",
         events: {
             "click .js-add-exercise": "addExerciseClicked",
-            "click .js-cancel": "cancelClicked"
+            "click .js-cancel": "cancelClicked",
+            "click .js-save": "saveClicked"
         },
 
         initialize: function () {
@@ -20,6 +21,27 @@ WorkoutTracker.module("ProgramsApp.Common.Views", function (Views, WorkoutTracke
 
         cancelClicked: function () {
             WorkoutTracker.trigger("programs:list");
+        },
+
+        saveClicked: function () {
+            var self = this;
+            var formData = Backbone.Syphon.serialize(this);
+            this.model.set(formData);
+
+            this.model.save(
+                null,
+                {
+                    success: function (model) {
+                        self.collection.each(function (item) {
+                            item.set("program_id", model.get("program_id"));
+                            item.unset("exercises");
+                            item.save();
+                        });
+                    }
+                }
+            );
+
+            WorkoutTracker.trigger("programs:list");
         }
     });
 
@@ -27,11 +49,16 @@ WorkoutTracker.module("ProgramsApp.Common.Views", function (Views, WorkoutTracke
         template: "#program-edit-exercises",
         className: "row",
         events: {
-            "click .js-remove-exercise": "removeExerciseClicked"
+            "click .js-remove-exercise": "removeExerciseClicked",
+            "change select": "setExercise"
         },
 
         onBeforeRender: function () {
             this.model.set("exercises", this.options.exercises);
+        },
+
+        setExercise: function (e) {
+            this.model.set("exercise_id", $(e.target).val());
         },
 
         removeExerciseClicked: function () {
--- app/assets/js/entities/program_exercise.js
+++ app/assets/js/entities/program_exercise.js
@@ -1,10 +1,22 @@
 WorkoutTracker.module("Entities", function (Entities, WorkoutTracker, Backbone, Marionette, $, _) {
     Entities.ProgramExercise = Backbone.Model.extend({
-        idAttribute: "program_exercise_id"
+        idAttribute: "program_exercise_id",
+
+        urlRoot: function () {
+            return "api/programs/" + this.get("program_id") + "/exercises";
+        },
+
+        defaults: {
+            program_id: 0
+        }
     });
 
     Entities.ProgramExerciseCollection = Backbone.Collection.extend({
-        model: Entities.ProgramExercise
+        model: Entities.ProgramExercise,
+
+        url: function () {
+            return "api/programs/" + this.at(0).get("program_id") + "/exercises";
+        }
     });
 
     var API = {
